2f5ea58f21e661983f30954f8023e07932dc2114
Nolan W. Brown <n-wbrown@users.noreply.github.com>
2017-12-21 11:41:29 -0800

    DEV/TST: add control features to scan_sequence

--- ease/alerts_engine/engine_tools/engine_base.py
+++ ease/alerts_engine/engine_tools/engine_base.py
@@ -10,9 +10,18 @@ class scan_sequence:
     This class lays out the basic tools for a repeated, time-regulated
     process that can be cancelled and altered asynchronously 
     """
+    end_code = "end"
+    upd_code = "upd"
+    end_msg = "done"
+    '''
+    def __new__(cls):
+        #msg_maps = {None:cls.operation}
+        return __new__(cls)
+    '''
     def __init__(self,delay=0):
         self.delay = delay
         self.queue = asyncio.Queue()
+        self.persist = True
 
 
     async def operation(self):
@@ -22,39 +31,66 @@ class scan_sequence:
         """
         raise NotImplementedError
 
-    async def regulator(self,future):
+
+    async def wait_next(self):
+        """
+        block until either the delay time has been reached or a message has
+        been received in the queue. Return message or none 
+        """ 
+        # if the delay is 0, checking the q. will always take longer and so
+        # must be manually checked 
+        if not self.queue.empty():
+            msg = True
+            message = await self.queue.get()
+        else:
+            msg = False
+        
+        done = set()
+        running = set()
+        if not msg:
+            done, running = await asyncio.wait(
+                [self.queue.get(),asyncio.sleep(self.delay)],
+                return_when = asyncio.FIRST_COMPLETED
+            )
+            completed = done.pop() 
+            message = completed.result()
+
+        #cleanly cancel which ever coro is still running (if any)
+        for r in running:
+            r.cancel()
+
+        return message
+
+    async def update(self,message):
+        raise NotImplementedError
+
+
+    async def message_handler(self,message):
+        print("starting message_handler")
+        if message == self.end_code:
+            self.persist = False
+        print("reached op if")
+        if message == None:
+            print("evoking op")
+            print(self.operation)
+            await self.operation()
+
+    async def start(self):
+        task = asyncio.ensure_future(self.regulator)
+        return task
+
+    async def cancel(self):
+        self.queue.put(self.end_code)
+
+    async def regulator(self):
         """
         regulator schedules the regular calls to the operation method and
         listens for interruptions.
         """
         running = set()
         done = set()
-        while True:
-            """
-            try:
-                message = self.queue.get_nowait()
-            except asyncio.QueueEmpty:
-                message = None
-            """
-            
-            done = set()
-            running = set()
-            try:
-                message = self.queue.get_nowait() 
-            except asyncio.QueueEmpty:
-                message = None
-            if not message:
-                done, running = await asyncio.wait(
-                    [self.queue.get(),asyncio.sleep(self.delay)],
-                    return_when = asyncio.FIRST_COMPLETED
-                )
-            for r in running:
-                r.cancel()
-
-            #done should be a single length set
-            result = done.pop()
-            if result.result() == "end":
-                future.set_result("done")
-                break
-            
-            await self.operation()
+        while self.persist:
+            print('running loop')
+            message = await self.wait_next()
+            print('in-loop message:',message)
+            await self.message_handler(message)
--- ease/alerts_engine/tests/test_engine_base.py
+++ ease/alerts_engine/tests/test_engine_base.py
@@ -10,8 +10,50 @@ def test_scan_sequence_init():
         pytest.fail("constructing new scan_sequence failed")
 
 
-def test_scan_sequence_regulator_repeated_operation():
+@pytest.mark.parametrize("msg", [1.2,{"abc":"def"},set([1,2,3])])
+def test_scan_sequence_wait_next(msg):
+    scanner = scan_sequence()
+    q = scanner.queue
 
+    async def test_mgr():
+        # ensure that delays timeout properly 
+        scanner.delay = .1
+        task = asyncio.ensure_future(scanner.wait_next())
+        try:
+            result = await asyncio.wait_for(task,timeout=.2)
+        except asyncio.TimeoutError:
+            pytest.fail("normal timeout took too long to complete")
+        
+        assert result == None, "normal timeout returned wrong value"
+
+        #ensure that interruption is possible
+        scanner.delay = 100
+        task = asyncio.ensure_future(scanner.wait_next())
+        await scanner.queue.put(msg)
+        try:
+            result = await asyncio.wait_for(task,timeout=2)
+        except asyncio.TimeoutError:
+            pytest.fail("interruption test took too long to complete")
+
+        assert result == msg, "interruption test returned wrong value"
+        
+        #ensure that interruption is possible even with 0 delay
+        scanner.delay = 0
+        await scanner.queue.put(msg)
+        task = asyncio.ensure_future(scanner.wait_next())
+        try:
+            result = await asyncio.wait_for(task,timeout=2)
+        except asyncio.TimeoutError:
+            pytest.fail("0-delay interruption test  took too long to complete")
+        
+        assert result == msg, ("0-delay interruption test returns wrong value")
+
+    loop = asyncio.get_event_loop()    
+    loop.run_until_complete(test_mgr())
+
+'''
+def test_scan_sequence_start():
+    
     class test_scan(scan_sequence):
         async def operation(self):
             self.n = self.n + 1
@@ -20,16 +62,41 @@ def test_scan_sequence_regulator_repeated_operation():
     scanner.delay= 0
     scanner.n = 0
 
-    async def mgr():
+    async def test_mgr():
         future = asyncio.Future()
-        task = asyncio.ensure_future(scanner.regulator(future))
+        task = asyncio.ensure_future(scanner.regulator())
         await asyncio.sleep(.1)
         task.cancel()
 
     loop = asyncio.get_event_loop()
     
-    loop.run_until_complete(mgr())
-    assert scanner.n > 0, "intended operation has not run"
+    loop.run_until_complete(test_mgr())
+    assert scanner.n > 0, "intended operation never ran"
+    '''
+
+
+def test_scan_sequence_regulator_repeated_operation():
+
+    class test_scan(scan_sequence):
+        async def operation(self):
+            print("running op")
+            self.n = self.n + 1
+    
+    scanner = test_scan()
+    scanner.delay= 0
+    scanner.n = 0
+
+    async def test_mgr():
+        future = asyncio.Future()
+        task = asyncio.ensure_future(scanner.regulator())
+        await asyncio.sleep(1)
+        task.cancel()
+
+    loop = asyncio.get_event_loop()
+    
+    loop.run_until_complete(test_mgr())
+    print("final n:",scanner.n)
+    assert scanner.n > 0, "intended operation never ran"
 
 
 
@@ -44,7 +111,7 @@ def test_scan_sequence_regulator_termination():
     scanner.n = 0
 
 
-    async def mgr():
+    async def test_mgr():
         future = asyncio.Future()
         task = asyncio.ensure_future(scanner.regulator(future))
         await asyncio.sleep(.1)
@@ -54,7 +121,7 @@ def test_scan_sequence_regulator_termination():
 
     loop = asyncio.get_event_loop()
     
-    loop.run_until_complete(mgr())
+    loop.run_until_complete(test_mgr())
     assert scanner.n > 0, "intended operation has not run"
     
 

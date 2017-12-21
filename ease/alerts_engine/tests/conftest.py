b38b52960dfa9954c1be58814a507297e74a2da9
Nolan W. Brown <n-wbrown@users.noreply.github.com>
2017-12-21 15:37:44 -0800

    adding conftest

---
+++ ease/alerts_engine/tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from engine_tools.engine_base import scan_sequence
+import logging
+
+logger = logging.getLogger(__name__)
+logger.propagate = False
+@pytest.fixture(scope='function')
+def test_scan():
+    class test_scan(scan_sequence):
+        def __init__(self):
+            super().__init__(self)
+            self.n=0
+            self.delay=0
+
+        async def operation(self):
+            self.n = self.n + 1
+            logger.debug("n: " + str( self.n))
+            
+    return test_scan()
